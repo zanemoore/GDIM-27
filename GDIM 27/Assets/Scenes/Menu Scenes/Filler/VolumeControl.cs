f660837f818ed1a5c235b19cd8275eecbc59e9cf
goozmatic <115593900+goozmatic@users.noreply.github.com>
2023-06-07 21:25:14 -0700

    minor catchup

--- GDIM 27/Assets/Scenes/Menu Scenes/Filler/VolumeControl.cs
+++ GDIM 27/Assets/Scenes/Menu Scenes/Filler/VolumeControl.cs
@@ -1,18 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class VolumeControl : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    FMOD.Studio.Bus AllSFX;
+
+    float allVolume = 0.5f;
+
+    void Awake()
     {
-        
+        AllSFX = FMODUnity.RuntimeManager.GetBus("bus:/All SFX");
     }
 
-    // Update is called once per frame
-    void Update()
+    public void MasterVolumeLevel(float newMasVol)
     {
-        
+        allVolume = newMasVol;
+        AllSFX.setVolume(allVolume);
     }
 }
